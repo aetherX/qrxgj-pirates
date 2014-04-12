8cdd0567ff161002a4c1b12fb4f37ef4e222256a
DigiM <degoldie_then@yahoo.com>
2014-04-12 22:06:38 +0700

    Commented the code

--- Pirates/src/qrxgj/pirates/main/Main.java
+++ Pirates/src/qrxgj/pirates/main/Main.java
@@ -1,6 +1,8 @@
 package qrxgj.pirates.main;
 
 public class Main {
+	/**Called when project is called, outputs the string Hello World!
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
